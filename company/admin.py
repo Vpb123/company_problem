21295b1b601aa7c39280d7557b52505187cd15dd
Vishwajit123 <vishwajit20bhagat@gmail.com>
2020-08-25 19:16:23 +0530

    my-models

---
+++ company/admin.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Company,Problems,Student,Solution,Sol_progress
+
+class CompanyAdmin(admin.ModelAdmin):
+
+    list_display=['C_name','Owner','C_type']
+    search_fields=['C_name']
+
+admin.site.register(Company,CompanyAdmin)
+
+class ProblemsAdmin(admin.ModelAdmin):
+
+    list_display=['problem','image']
+
+
+admin.site.register(Problems,ProblemsAdmin)
+class StudentAdmin(admin.ModelAdmin):
+
+    list_display=['Name','mob_no','branch']
+    search_fields=['branch','Name']
+
+admin.site.register(Student,StudentAdmin)
+
+class SolutionAdmin(admin.ModelAdmin):
+    list_display=['S_name','sol_name','Problem']
+
+admin.site.register(Solution,SolutionAdmin)
+
+class ProgressAdmin(admin.ModelAdmin):
+
+    list_display=['sol','progress']
+
+admin.site.register(Sol_progress,ProgressAdmin)
---
+++ company/models.py
@@ -0,0 +1,63 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+         pass
+
+# Create your models here.
+class Company(models.Model):
+    user=models.ForeignKey('User',on_delete=models.CASCADE)
+    C_name=models.CharField(max_length=50)
+    Owner=models.CharField(max_length=50)
+    C_type=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.C_name
+
+class Problems(models.Model):
+        company=models.ForeignKey("Company",on_delete=models.CASCADE)
+        problem=models.TextField(max_length=400)
+        image=models.URLField()
+        video=models.URLField()
+
+        class Meta:
+                ordering=['problem']
+
+        def __str__(self):
+                return self.problem
+class Student(models.Model):
+        user=models.ForeignKey('User',on_delete=models.CASCADE)
+        Name=models.CharField(max_length=20)
+        mob_no=models.CharField(max_length=12)
+        branch=models.CharField(max_length=20)
+        state=models.CharField(max_length=20)
+        district=models.CharField(max_length=20)
+        Address=models.TextField(max_length=300)
+        problems=models.ManyToManyField(Problems)
+
+        class Meta:
+                ordering=['Name']
+
+        def __str__(self):
+            return self.Name
+
+class Solution(models.Model):
+        user=models.ForeignKey("User",on_delete=models.CASCADE)
+        S_name=models.ForeignKey("Student",on_delete=models.CASCADE)
+        Problem=models.ForeignKey('Problems',on_delete=models.CASCADE)
+        From_date=models.DateTimeField()
+        TO_date=models.DateTimeField()
+        sol_name=models.CharField(max_length=10)
+
+        def __str__(self):
+                return f'{self.sol_name} -> {self.user}'
+
+class Sol_progress(models.Model):
+        sol=models.ForeignKey("Solution",on_delete=models.CASCADE)
+        progress=models.IntegerField()
+        progress_details=models.TextField()
+        image=models.URLField
+        video=models.URLField()
+
+        def __str__(self):
+            return self.sol
